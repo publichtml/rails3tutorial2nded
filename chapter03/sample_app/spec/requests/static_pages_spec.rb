c6e5beb3e87dd5d3eb3b1033e3027b7a7c6fa810
Wakana Odagiri <publi9html@gmail.com>
2013-11-30 19:07:03 +0900

    3.2 Add help messages to /static_pages/help

--- chapter03/sample_app/spec/requests/static_pages_spec.rb
+++ chapter03/sample_app/spec/requests/static_pages_spec.rb
@@ -7,4 +7,11 @@ describe "Static Pages" do
       page.should have_content('Sample App')
     end
   end
+
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
+    end
+  end
 end
