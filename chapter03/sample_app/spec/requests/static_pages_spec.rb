493df1fa949465bc1fe73af25ba605078fe4c086
Wakana Odagiri <publi9html@gmail.com>
2013-12-07 17:13:47 +0900

    3.5 Remove text duplication in spec by using let

--- chapter03/sample_app/spec/requests/static_pages_spec.rb
+++ chapter03/sample_app/spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home Page" do
     it "should have the h1 'Sample App'" do
       visit 'static_pages/home'
@@ -9,7 +11,7 @@ describe "Static Pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Home")
+      page.should have_title("#{base_title} | Home")
     end
   end
 
@@ -21,7 +23,7 @@ describe "Static Pages" do
 
     it "should have the title 'Help'" do
       visit 'static_pages/help'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+      page.should have_title("#{base_title} | Help")
     end
   end
 
@@ -33,7 +35,7 @@ describe "Static Pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_title("#{base_title} | About Us")
     end
   end
 
@@ -45,7 +47,7 @@ describe "Static Pages" do
 
     it "should have the title 'Contact'" do
       visit 'static_pages/contact'
-      page.should have_title("Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_title("#{base_title} | Contact")
     end
   end
 end
